1f56a641c5957bb3dfc3370c5e19fb4d557edd7e
DiceRandom <51027869+DiceRandom@users.noreply.github.com>
2023-01-18 17:13:57 -0500

    cleanup after directory fix

--- registerservice.js
+++
@@ -1,6 +0,0 @@
-navigator.serviceWorker.register('background.js').then(function(registration) {
-    console.log('ServiceWorker registration successful with scope: ', registration.scope);
-         }, function(err) {
-    console.log('ServiceWorker registration failed: ', err);
-
-});
